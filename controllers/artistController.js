5c441ee86a17cec736d27c45f90c39e13069494e
ruhulcse <ruhul.cse7862@gmail.com>
2023-03-30 18:14:28 +0600

    api final

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -1,38 +1,63 @@
 const asyncHandler = require("express-async-handler");
-const axios = require('axios');
+const axios = require("axios");
+const fs = require('fs');
+const path = require('path');
+const csvWriter = require('csv-writer').createObjectCsvWriter;
 //Login for Users
-const callLastFMApi = async() =>{
+const callLastFMApi = async (name) => {
   try {
-    const apiUrl = ' https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=cher&api_key=ada165e490f08a5fb48e6945c805519d&format=json';
+    const apiUrl = `https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${name}&api_key=ada165e490f08a5fb48e6945c805519d&format=json`;
     const postData = {
       name: "test",
     };
-    const {data} = await axios.post(apiUrl, postData);
-    const artist = data.results.artistmatches
-    console.log(artist)
-    // Handle the API response and send a response to the client
-    // res.send(response.data);
+    const { data } = await axios.post(apiUrl, postData);
+   
+    const artist = data.results.artistmatches;
+    return artist;
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Error calling external API');
+    console(error);
+    res.status(500).send("Error calling external API");
   }
- 
-}
+};
 const getArtistInfo = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
-  await callLastFMApi()
-  console.log("api key")
-  // const user = await User.findOne({ email });
-  // if (user && (await user.matchPassword(password))) {
-  //   res.json({
-  //     message: "login success",
-  //     _id: user._id,
-  //     email: user.email,
-  //     token: generateToken(user._id),
-  //   });
-  // } else {
-  //   res.status(202).send(new Error("invalid user name or password"));
-  // }
+  const { name } = req.body;
+  try {
+    const response = await callLastFMApi(name);
+    if (response) {
+      const artists = response.artist;
+      const parsedData = artists.map(artist => {
+        const smallImage = artist.image.find(img => img.size === 'small');
+        return {
+          name: artist.name,
+          mbId: artist.mbid,
+          url: artist.url,
+          image_small: smallImage ? smallImage['#text'] : '',
+        };
+      });
+
+      // Write to CSV
+      const csvFilePath = path.join(__dirname,'..', 'public', `${name}.csv`);
+      const writer = csvWriter({
+        path: csvFilePath,
+        header: [
+          { id: 'name', title: 'name' },
+          { id: 'mbId', title: 'mbid' },
+          { id: 'url', title: 'url' },
+          { id: 'image_small', title: 'image_small' },
+        ],
+      });
+      await writer.writeRecords(parsedData);
+      res.json({
+        message: "successfully retrived data",
+        data: response
+      });
+    } else {
+      res.status(202).send(new Error("No data found"));
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).send("Error calling external API");
+  }
 });
 
 module.exports = {
