903fbc27af807105f4f77220c0d4b9508e972669
Ruhul amin <ruhul.cse7862@gmail.com>
2023-07-12 16:22:11 +0600

    Update mail.js

--- helpers/mail.js
+++ helpers/mail.js
@@ -1,24 +1,13 @@
 const nodemailer = require("nodemailer");
 
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: process.env.APP_EMAIL,
-//     pass: process.env.APP_EMAIL_PASS,
-//   },
-// });
-// SMTP Server(Rackspace Email Hosting): secure.emailsrvr.com
-// SMTP port: 587
-// SMTP security: TLS
-// SMTP username: contact@completegreet.com
-// SMTP password: 1212Fiverr
+
 
 const transporter = nodemailer.createTransport({
   host: "secure.emailsrvr.com",
   port: 587,
   auth: {
-    user: "support@completegreet.com",
-    pass: "Fiverr159",
+    user: "username",
+    pass: "password",
   },
 });
 
