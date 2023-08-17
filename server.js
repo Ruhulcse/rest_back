d21b06cdabf7eacac93d8bdbc7f51a21d714b1ac
Ruhul Amin <ruhul.amin@apsissolutions.com>
2023-08-17 21:15:03 +0600

    server updated

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get("/", function (req, res) {
 
 app.use("/api/v1", artistRoutes);
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 22;
 app.listen(
   PORT,
   console.log(
